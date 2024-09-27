dbc9281c86d5b609031552bf3b4566889cf4b52e
tinpo <137786541+timnyphop@users.noreply.github.com>
2024-09-27 22:36:27 +0700

    delete sort add sort on backend

---
+++ src/app/components/preloader/preloader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { PreloadAllModules } from '@angular/router';
+
+@Component({
+  selector: 'app-preloader',
+  standalone: true,
+  imports: [],
+  templateUrl: './preloader.component.html',
+  styleUrl: './preloader.component.scss',
+})
+export class PreloaderComponent {}
--- src/app/pages/raiting/raiting.component.ts
+++ src/app/pages/raiting/raiting.component.ts
@@ -4,37 +4,24 @@ import { ISpecialnost } from '../../../types/specialnost';
 import { TestHttpComponent } from '../../components/test-http/test-http.component';
 import { AbiturientsService } from '../../services/abiturients.service';
 import { Component } from '@angular/core';
+import { PreloaderComponent } from '../../components/preloader/preloader.component';
 
 @Component({
   selector: 'app-raiting',
-  imports: [CommonModule, SlicePipe, TestHttpComponent],
+  imports: [CommonModule, SlicePipe, TestHttpComponent, PreloaderComponent],
   templateUrl: './raiting.component.html',
   styleUrls: ['./raiting.component.scss'],
   standalone: true,
 })
 export class RaitingComponent {
-  abiturients: IAbiturient[] = [];
-  specialnosti: ISpecialnost[] = [];
+  abiturients: IAbiturient[];
+  specialnosti: ISpecialnost[];
 
   constructor(private abiturientsService: AbiturientsService) {
     abiturientsService.getAllAbiturientsBySpecialnost().subscribe((data) => {
-      this.specialnosti = data; // Предполагается, что data - это массив ISpecialnost
-      this.specialnosti.forEach((specialnost) => {
-        specialnost.abiturients = this.sortStudent(specialnost.abiturients); // Сортируем абитуриентов для каждой специальности
-      });
+      this.specialnosti = data; // Предполагается, что data - это массив ISpecialnost с отсортированными абитуриентами
     });
   }
 
   // Сортировка абитуриентов по специальности
-  sortStudent(abiturients: IAbiturient[]): IAbiturient[] {
-    return abiturients
-      .sort((a, b) => {
-        if (b.originalsertificate && !a.originalsertificate) return 1;
-        if (a.originalsertificate && !b.originalsertificate) return -1;
-        if (b.svo && !a.svo) return 1;
-        if (a.svo && !b.svo) return -1;
-        return b.averagescore - a.averagescore;
-      })
-      .slice(0, 50); // Срезаем до 50 абитуриентов
-  }
 }
